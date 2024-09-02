4a956d151ca89a2d208e5464174c0d7b3afeee54
MirandaNichols123 <mnichols10@cub.uca.edu>
2024-09-02 17:15:42 -0500

    adding to purse and making register class

--- src/Lab1/Purse.java
+++ src/Lab1/Purse.java
@@ -18,7 +18,7 @@ public class Purse
     public double remove(Denomination type, int num)
     {
         //checks if denomination exists
-        if(cash.containsKey(type))
+        if (cash.containsKey(type))
         {
             //current number of denomination
             int currentNum = cash.get(type);
@@ -26,9 +26,32 @@ public class Purse
             {
                 num = currentNum;
             }
-            //if denomination DNE i purse return 0.0
-            return 0.0;
+            //updates map by subtracting the removed number of denomination
+            cash.put(type, cash.get(type) - num);
+            return type.amt() * num;
         }
-
+        //if denomination DNE i purse return 0.0
+        return 0.0;
     }
+    //Method to calculate the total value of money in the purse
+    public double getValue()
+    {
+        return cash.entrySet().stream()//stream of 'cash' map entry
+                .mapToDouble(e->e.getKey().amt() * e.getValue())//maps each entry to corresponding value by multiplying denomination value
+                .sum();
+    }
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        //iterates through map and append denomination anmes and qualities
+        cash.forEach((denom, count)->
+        {
+            if(count>0)
+            {
+                sb.append(count).append(" ").append(denom.name()).append("\n");
+            }
+        });
+        return !sb.isEmpty() ? sb.toString() : "Empty Purse";
+    }
+
 }
